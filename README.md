d27dad46100a39f497d5e4f4f1a25a166558c49a
KEKO <s.rodrigueznavarro@gmail.com>
2024-10-11 23:32:50 +0200

    dia1

--- README.md
+++ README.md
@@ -16,12 +16,13 @@ El juego termina cuando:
 
 El jugador adivina correctamente la palabra (gana)
 
-El jugador agota todos los intentos sin adivinar la palabra (pierde)
+# El jugador agota todos los intentos sin adivinar la palabra (pierde)
 
-La palabra seleccionada al azar debe mostrarse como guiones bajos _ que representen las letras no adivinadas. Por ejemplo, si la palabra es python, se verá como _ _ _ _ _ _
+# La palabra seleccionada al azar debe mostrarse como guiones bajos _ que representen las letras no adivinadas. Por ejemplo, si la palabra es python, se verá como _ _ _ _ _ _
 
 Si el jugador adivina una letra correctamente, el guión correspondiente a esa letra será reemplazado por la letra adivinada.
 
 El jugador puede intentar adivinar letra varias veces, pero no debe contar como intento fallido si ya ha adivinado esa letra.
 
-Al finalizar el juego, el programa debe indicar si el jugador ganó o perdió y mostrar la palabra correcta si pierde.
+Al finalizar el juego, el programa debe indicar si el jugador ganó o 
+# perdió y mostrar la palabra correcta si pierde.
--- ahorcado.py
+++ ahorcado.py
@@ -1,19 +1,27 @@
 from random import * #para poder sacar aleatorios de las listas
 
 palabras = ['perro', 'luz', 'cielo', 'programar'] #guardamos las palabras que queremos usar 
-aleatorio = choice(palabras) #para sacar un numero aleatioro de los que estan guardados
+aleatorio = choice(palabras) #para sacar una palabra aleatioria de palabras
+numletras = len(aleatorio)  # Contamos el número de letras en aleatorio
 
 
+print("La palabra secreta es: ",aleatorio, "\nNumero de letras: ",numletras,) #solo para saber que palabra es para las pruebas
 
-print("La palabra secreta es " ,aleatorio, ) #solo para saber que palabra es para las pruebas
+print(" - " * numletras) # Pintamos una linea por cada letra
 
 intentos = 5;
-letra = input ("mete una letra: ") #le pedimos que meta una letra para empezar a jugar
 
-while intentos >= 1:
-    print(f"No esta, te quedan {intentos} intentos mas")
-    intentos -= 1
-    letra = input ("mete una letra") #le pedimos que meta cuando falle y le queden oportunidades
+letras = list(aleatorio) #asi guardamos en letras todas las letras de la palabra aleatoria
+print (letras) #aqui estan todas las letras separadas
+
+letra = input ("mete una letra: ") #le pedimos que meta una letra para empezar a jugar
+while intentos >= 1: #Si le quedan intentos entra y mira si esta la letra o no
+    if letra == letras:
+          letra = input ("CORRECTO!! mete orta letra: ") #le pedimos que meta otra cuando acierte y le queden oportunidades
+    else:      
+            print(f"{letra} No esta, te quedan {intentos} intentos mas")
+            intentos -= 1
+            letra = input ("mete una letra: ") #le pedimos que meta cuando falle y le queden oportunidades
 
 else:
-    print("PERDISTE")  # Cuando tienes 6 fallos dice que perdiste
+    print("PERDISTE......La palabra que se escondia es: ",aleatorio,)  # Cuando tienes 6 fallos dice que perdiste
